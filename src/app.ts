1faa7be95854bd9b77e2eb13ba0942c0aec5c2d4
grzegorz.socha <grzegorz.socha@farmaprom.pl>
2023-04-09 21:57:03 +0200

    remove unnecessary pageScraper

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import { launch } from "puppeteer";
-import scraper from "./scraper/pageScraper";
+import getConstructionKit from "./scraper/set";
+import { getSets } from "./service/db";
+
+const CONSTRUCTION_KIT_ROW_SELECTOR = ".kit.views-row";
+const EXTENSION_ROW_SELECTOR = ".expansion.views-row";
 
 export default async function run() {
   const browser = await launch({
@@ -10,7 +14,14 @@ export default async function run() {
 
   const page = await browser.newPage();
 
-  await scraper(page);
+  await getConstructionKit(
+    page,
+    CONSTRUCTION_KIT_ROW_SELECTOR,
+    "construction_kit"
+  );
+  await getConstructionKit(page, EXTENSION_ROW_SELECTOR, "extension");
+
+  // const sets = getSets(page);
 
   await page.close();
   await browser.close();
